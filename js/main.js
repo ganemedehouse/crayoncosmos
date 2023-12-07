655d0ce76b52e888a37d63e4b5b3b1b2a227f089
Ganymedehouse House <ganymedehousestudio@gmail.com>
2023-12-07 11:59:23 +0400

    added realistic typing for the title

--- js/main.js
+++ js/main.js
@@ -9,25 +9,66 @@ const titleVariations = [
     "Art as a Mirror of the Soul",
     "Exploring the Creative Mind",
     "Unveiling the Genius Within",
+    "Unveiling the Child Within",
+    "A Kaleidoscope of Creativity",
+    "A Glimpse into the Unfiltered Canvas",
+    "Where Imagination Takes Flight",
+    "The Artful Journey of Childhood",
+    "The Language of Crayons",
+    "Mindful Marks on Paper",
+    "Sparkling with Creative Energy",
+    "Beyond the Lines and Colors",
+    "The Unbound Potential of Imagination",
 ];
 
 function init() {
-    var randomIndex = Math.floor(Math.random() * titleVariations.length);
-    var titleElement = document.getElementById("title");
-    var characters = titleVariations[randomIndex].split("");
-    var currentCharIndex = 0;
-    var cursorElement = document.getElementById("cursor");
+    typeTitle();
+}
 
-    var intervalId = setInterval(function () {
+function typeTitle() {
+    const randomIndex = Math.floor(Math.random() * titleVariations.length);
+    const titleElement = document.getElementById("title");
+    const cursorElement = document.getElementById("cursor");
+    const characters = titleVariations[randomIndex].split("");
+    let currentCharIndex = 0;
+
+    function typeNextChar() {
         if (currentCharIndex >= characters.length) {
-            clearInterval(intervalId);
-            cursorElement.style.animation = "cursor-blink 1s  infinite";
+            cursorElement.style.animation = "cursor-blink 1s infinite";
+            setTimeout(function () {
+                cursorElement.style.animation = "none";
+                deleteTitle();
+            }, 5000); // 5 Seconds
         } else {
-            var newCharacter = document.createTextNode(characters[currentCharIndex]);
+            const newCharacter = document.createTextNode(
+                characters[currentCharIndex]
+            );
             titleElement.insertBefore(newCharacter, cursorElement);
             currentCharIndex++;
+            setTimeout(typeNextChar, Math.floor(30 + Math.random() * 120)); // 30-150 milliseconds
         }
-    }, 50);
+    }
+
+    typeNextChar();
+}
+
+function deleteTitle() {
+    const titleElement = document.getElementById("title");
+    const characters = Array.from(titleElement.childNodes);
+    let currentCharIndex = characters.length - 1;
+
+    const intervalId = setInterval(function () {
+        if (currentCharIndex < 0) {
+            clearInterval(intervalId);
+            typeTitle();
+        } else {
+            if (characters[currentCharIndex].nodeType === Node.TEXT_NODE) {
+                // Only delete if it's a text node
+                titleElement.removeChild(characters[currentCharIndex]);
+            }
+            currentCharIndex--;
+        }
+    }, 30);
 }
 
 init();
--- readme.md
+++ readme.md
@@ -1,1 +1,30 @@
-# Crayon Cosmos
+## Crayon Cosmos: A Portfolio of Child Art and Exploration
+
+**Welcome to Crayon Cosmos, a platform dedicated to celebrating the vibrant art of children and exploring its fascinating psychological depths.** Here, you'll discover a captivating gallery of artwork, delve into the hidden meanings behind children's creations, and gain valuable insights from leading experts.
+
+### What awaits you:
+
+-   **A captivating gallery:** Immerse yourself in a diverse collection of children's artwork, spanning various age groups, mediums, and styles.
+-   **Psychological insights:** Uncover the hidden meanings behind children's art, understanding how their creations reflect their emotions, thoughts, and developmental stages.
+-   **Expert knowledge:** Delve into the research conducted by leading psychologists and art educators, gaining valuable insights into the crucial role of art in children's development.
+-   **Inspirational stories:** Be inspired by the journeys of young artists, their creative processes, and their artistic triumphs.
+-   **Engaging resources:** Find helpful resources for parents, educators, and anyone who wants to learn more about child art and its significance.
+
+**Whether you're an art enthusiast, a child development specialist, or simply someone who appreciates the beauty and wonder of children's imaginations, Crayon Cosmos offers a unique and captivating experience.**
+
+### Contributing to Crayon Cosmos
+
+Crayon Cosmos is an open-source project dedicated to fostering a community around child art and its exploration. We welcome contributions in various forms, including:
+
+-   **Artwork submissions:** Share your own children's artwork or the work of young artists you know.
+-   **Expert insights:** Contribute articles, research summaries, or other resources related to child art and its psychological significance.
+-   **Code contributions:** Help us improve and expand the website by contributing to the codebase.
+-   **Feedback and suggestions:** We value your feedback and suggestions to make Crayon Cosmos a more valuable resource for everyone.
+
+Please visit our Contribution Guidelines: CONTRIBUTING.md for more information on how to get involved.
+
+### License
+
+Crayon Cosmos is released under the CC0 1.0 Universal Public Domain Dedication: LICENSE. This means that you are free to use, modify, and distribute the website and its contents without any restrictions.
+
+**We hope you enjoy exploring Crayon Cosmos!**
